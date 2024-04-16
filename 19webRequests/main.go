3719e4830ff85abb4f5c755607c875b6771c5e44
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-04-16 12:35:47 +0530

    web req

---
+++ 19webRequests/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const url = "https://lco.dev"
+
+func main() {
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("res is %T\n", res) // *Http.Response , astrik means original not copy
+
+	defer res.Body.Close() // it is callers responsibilty to close the connec
+
+	dataBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(dataBytes))
+
+}
